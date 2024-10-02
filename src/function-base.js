2f45191be958829db8b8fd42ff517b4f1458f421
IT22056320 <127720899+IT22056320@users.noreply.github.com>
2024-10-02 20:17:23 +0530

    Add function-base.js from app

---
+++ src/function-base.js
@@ -0,0 +1,13 @@
+// Simple addition function
+function add(a, b) {
+    return a + b; // Return sum
+}
+
+// Simple subtraction function
+function subtract(a, b) {
+    return a - b; // Return difference
+}
+
+// Print the result
+console.log(add(5, 3));
+console.log(subtract(5, 3)); 
